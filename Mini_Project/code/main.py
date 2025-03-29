1a4b9d808963a1892b4b92fe5ff10a1e79f22a55
jkell <30220+jkell@users.noreply.github.mit.edu>
2025-03-29 12:01:42 -0400

    adding code to make latex params

--- Mini_Project/code/main.py
+++ Mini_Project/code/main.py
@@ -21,6 +21,15 @@ reload(data)
 with open("params.toml", "r") as file:
     params = toml.load(file)
 
+# Write to a .tex file
+with open("../variables.tex", "w") as f:
+    for key, value in params.items():
+        if isinstance(value, str):
+            f.write(f"\\newcommand{{\\{key}}}{{{value}}}\n")
+        else:
+            f.write(f"\\newcommand{{\\{key}}}{{{value}}}\n")
+
+print("variables.tex has been generated!") 
 DD = data.DiamondData(params)
 DD.simulate()
 # %%
